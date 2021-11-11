1fdeb58d63d79cee84dd35d6a3572eb548d516e5
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-12 03:57:44 +0500

    turned off cache control

--- pages/api/groups/index.ts
+++ pages/api/groups/index.ts
@@ -16,7 +16,7 @@ const groups: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse)
 
   if (error) throw new Error(`${error.message} (hint: ${error.hint})`);
 
-  res.setHeader('Cache-Control', ['public', 'maxage=21600', 's-maxage=21600', 'stale-while-revalidate=21600']);
+  // res.setHeader('Cache-Control', ['public', 'maxage=21600', 's-maxage=21600', 'stale-while-revalidate=21600']);
 
   res.status(200);
   res.json({ options: { limit, cursor, search }, results: Groups });
